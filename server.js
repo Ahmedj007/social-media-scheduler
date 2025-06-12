c72f9b58504f6295a3891b892a0c3a02ceaeeea0
Ahmed <Ahmedhazm007@gmail.com>
2025-06-12 22:48:59 +0300

    setting up server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false }));
+
+//Set view engines
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+app.get('/',(req,res)=>{
+    res.send(`API is running....`);
+});
+
+app.listen(3000,()=>{
+    console.log('app is running on http://localhost:3000');
+})
